a5b281467fbf3c143718cb59a286bcfd44f202dc
Anton Licht <anton.licht@gmx.de>
2013-11-26 17:16:24 +0100

    Project Setup

---
+++ Assets/Scripts/MapTest/CompassTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CompassTest : MonoBehaviour
+{
+
+    public float Latitude;
+    public float Longitude;
+    public MapGrid Grid;
+
+    public const float moveSpeed = 0.003f;
+
+    void Start()
+    {
+        Latitude = 52.50451f;
+        Longitude = 13.39699f;
+        //Input.location.Start();
+        //Input.compass.enabled = true;
+
+    }
+
+    void Update()
+    {
+        //if (Input.location.status != LocationServiceStatus.Running)
+        //    return;
+        //SetLocation();
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            Longitude -= touch.deltaPosition.x * moveSpeed * moveSpeed;
+            Latitude -= touch.deltaPosition.y * moveSpeed * moveSpeed;
+        }
+        Grid.CurrentPosition = MapUtils.GeographicToProjection(new Vector2(Longitude, Latitude), 18);
+    }
+
+
+    private void SetLocation()
+    {
+        Longitude = Input.location.lastData.longitude;
+        Latitude = Input.location.lastData.latitude;
+    }
+
+}
---
+++ Assets/Scripts/MapTest/RenderCell.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class RenderCell : MonoBehaviour, ICell<RefCountedSprite>
+{
+    public RefCountedSprite Content;
+    private SpriteRenderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        if (!_renderer)
+            _renderer = gameObject.AddComponent<SpriteRenderer>();
+    }
+
+    public RefCountedSprite GetContent()
+    {
+        return Content;
+    }
+
+    public void SetContent(RefCountedSprite content)
+    {
+        if (content == Content)
+            return;
+        if (Content != null)
+        {
+            Content.Release();
+            Content.SpriteChanged -= OnSpriteLoaded;
+        }
+        Content = content;
+        if (Content == null)
+        {
+            _renderer.sprite = null;
+            return;
+        }
+        Content.SpriteChanged += OnSpriteLoaded;
+        _renderer.sprite = Content.Sprite;
+        Content.AddRef();
+    }
+
+    private void OnSpriteLoaded(Sprite sprite)
+    {
+        _renderer.sprite = Content.Sprite;
+    }
+
+    public void Clear()
+    {
+        //Content = null;
+    }
+}
